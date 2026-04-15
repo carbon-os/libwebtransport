8a13ae2ca5274e23f9dcd409f06361844260324b
carbon-os <259760037+carbon-os@users.noreply.github.com>
2026-04-14 17:39:17 -0700

    updates

--- ports/webtransport/portfile.cmake
+++ ports/webtransport/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO carbon-os/libwebtransport
     REF v1.0.0 
-    SHA512 0
+    SHA512 d6358585120c90f5ae1e80d718c7845367c41339a2527b8a4c994f3b0f1ff2e9280d19ec29f7d128e247b823824890c6835d230db71d3997f1ff9f1f4f0ee63f
     HEAD_REF main
 )
 
